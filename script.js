9994d21808a17e7607e0a2625038b0c67f811230
priyanshuup <109269025+priyanshuup@users.noreply.github.com>
2023-07-04 21:59:46 +0530

    tgiff

---
+++ script.js
@@ -0,0 +1,53 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  centeredSlides: false,
+  fade: 'true',
+  loop: true,
+  grabCursor: 'true',
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+
+breakpoints:{
+  0: {
+      slidesPerView:1,
+  },
+  767: {
+      slidesPerView:2,
+  },
+  950: {
+      slidesPerView:3,
+  },
+  1400: {
+    slidesPerView:4,
+  }
+  },
+});
